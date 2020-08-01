3c84b91f1ad6cf6af3e3983b42e6d0c2ca370908
Tony Tony <tongwanhua1993@163.com>
2020-08-01 15:30:57 +0800

    Initial Commit

---
+++ GITDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GITDemo
+//
+//  Created by 童万华 on 2020/8/1.
+//  Copyright © 2020 mucang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
